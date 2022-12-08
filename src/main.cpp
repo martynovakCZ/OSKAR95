af2154c7c815e102b53a51332ef2420455c5a915
Martin Novák <nov-martin@centrum.cz>
2022-12-08 15:48:30 +0100

    testing move arm to all maximums

--- src/main.cpp
+++ src/main.cpp
@@ -442,13 +442,15 @@ extern "C" void app_main(void)
     vTaskDelay(500/portTICK_PERIOD_MS);
 
     //synchronizeMotor(driver0, opto0, 1);
-    //checkOptos();
 
     IndexStepCounter_init(PCNT_UNIT_0, GPIO_NUM_12, GPIO_NUM_0);
     IndexStepCounter_init(PCNT_UNIT_1, GPIO_NUM_18, GPIO_NUM_0);
     IndexStepCounter_init(PCNT_UNIT_2, GPIO_NUM_15, GPIO_NUM_0);
     IndexStepCounter_init(PCNT_UNIT_3, GPIO_NUM_13, GPIO_NUM_0);
     movePosition(360, 360, 360, 360, -1, -1, 1, 1, driver0, driver1, driver2, driver3);
+    vTaskDelay(500/portTICK_PERIOD_MS);
+    movePosition(35*360, 5*360, 36*0, 12*360, -1, -1, 1, 1, driver0, driver1, driver2, driver3); // maximalni stupne: 40, 5, 36, 12 // podstava, klepeta, loket, rameno // -1 od opto, -1 od opto, 1 od opto, 1 od opto
+
     return;
    
 }
