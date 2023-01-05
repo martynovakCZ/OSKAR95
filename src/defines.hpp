9254c328af0d9a8d8357d28f7341f1a7d5904d85
Martin Novák <nov-martin@centrum.cz>
2023-01-05 19:58:04 +0100

    made motor_speed0 instead of moto_speed4

--- src/defines.hpp
+++ src/defines.hpp
@@ -42,7 +42,7 @@
 volatile int motor_speed1 = MOTOR_SPEED_COEFICIENT/3;      //svirani klepete
 volatile int motor_speed2 = MOTOR_SPEED_COEFICIENT/3;      //rotace zakladny
 volatile int motor_speed3 = MOTOR_SPEED_COEFICIENT/3;      //zdvih ramene s klepety
-volatile int motor_speed4 = MOTOR_SPEED_COEFICIENT/3;      //zdvih celeho ramene
+volatile int motor_speed0 = MOTOR_SPEED_COEFICIENT/3;      //zdvih celeho ramene
 volatile int motor_speed = MOTOR_SPEED_COEFICIENT/3;       //testovaci motorspeed
 volatile int motor_load = 0;
 volatile int motor_stop_sensitivity = 100;
